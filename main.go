3d5c3fa06a7aef7a3a75c0ffcde09f47cff164ea
kakke18 <k55soc.rad@gmail.com>
2020-12-13 17:33:21 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type payload struct {
+	Text string `json:"text"`
+}
+
+func main() {
+	// webhook URLを取得
+	webhookURL := os.Getenv("WEBHOOK_URL")
+
+	// 送信データを生成
+	data, err := json.Marshal(payload{Text: "test"})
+	if err != nil {
+		fmt.Printf("[Error] %v.¥n", err.Error())
+		return
+	}
+
+	// POST
+	resp, err := http.PostForm(webhookURL, url.Values{"payload": {string(data)}})
+	if err != nil {
+		fmt.Printf("[Error] %v.¥n", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+}
